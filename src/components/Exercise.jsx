2b806acdd0784a9f9ff87be05aa449691a7bddeb
Jaysen <jaysenhdezlopez@gmail.com>
2024-05-06 11:42:29 -0400

    boton de compeltar y eliminar funcionando

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -2,21 +2,21 @@ import { useContext } from "react"
 import { WorkoutContext } from "./WorkoutContext"
 
 function Exercise(){
-    const { exercises } = useContext(WorkoutContext)
+    const { exercises, completeSet, removeExercise } = useContext(WorkoutContext)
 
     return(
         <div className="exerciseWrapper">
-            {exercises.map((exercise, index)=>(
-                <section key={index} className="exerciseComponent">
-                <h1>{exercise.nombre_ejercicio}</h1>
-                <h2>{exercise.equipo}</h2>
-                <h2>{exercise.peso} Lib</h2>
-                <h2>{exercise.repeticiones}</h2>
-                <h2>0 De {exercise.sets}</h2>
+            {exercises.map((exercise)=>(
+                <section key={exercise.id} className="exerciseComponent">
+                <h2><strong className="text-white">Ejercicio:</strong> {exercise.nombre_ejercicio}</h2>
+                <h2><strong className="text-white">Equipo:</strong> {exercise.equipo}</h2>
+                <h2><strong className="text-white">Peso:</strong> {exercise.peso} Lib</h2>
+                <h2><strong className="text-white">Reps:</strong> {exercise.repeticiones}</h2>
+                <h2><strong className="text-white">{exercise.completedSets} De:</strong> {exercise.sets}</h2>
                 {/* <h2>MOOD: {exercise.mood}</h2> */}
                 <div className="exerciseComponentBtnBox">
-                    <button className="btn btn-success">Completar Set</button>
-                    <button className="btn btn-danger">Eliminar Ejercicio</button>
+                    <button className="btn btn-success" onClick={()=> completeSet(exercise.id)}>Completar Set</button>
+                    <button className="btn btn-danger" onClick={()=> removeExercise(exercise.id)}>Eliminar Ejercicio</button>
                 </div>
                 </section>
             ))}
--- src/components/WorkoutContext.jsx
+++ src/components/WorkoutContext.jsx
@@ -1,17 +1,35 @@
 // WorkoutContext.js
 import React, { createContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export const WorkoutContext = createContext();
 
 export const WorkoutProvider = ({ children }) => {
     const [exercises, setExercises] = useState([]);
 
-    const addExercise = exercise => {
-        setExercises(prevExercises => [...prevExercises, exercise]);
+    const addExercise = (exercise) => {
+        const newExercise = {
+            ...exercise,
+            id: uuidv4(),  // Agregar un ID único a cada nuevo ejercicio
+            completedSets: 0
+        };
+        setExercises(prevExercises => [...prevExercises, newExercise]);
+    };
+
+    const completeSet = (id) => {
+        setExercises(prevExercises =>
+            prevExercises.map(exercise => 
+                exercise.id === id ? { ...exercise, completedSets: exercise.completedSets + 1 } : exercise
+            )
+        );
+    };
+
+    const removeExercise = (id) => {
+        setExercises(prevExercises => prevExercises.filter(exercise => exercise.id !== id));
     };
 
     return (
-        <WorkoutContext.Provider value={{ exercises, addExercise }}>
+        <WorkoutContext.Provider value={{ exercises, addExercise, completeSet, removeExercise }}>
             {children}
         </WorkoutContext.Provider>
     );
