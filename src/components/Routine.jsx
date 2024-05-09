1a2bb61cfabc1df9d7cd2d945ec8fdc431760cd5
Jaysen <jaysenhdezlopez@gmail.com>
2024-05-09 18:46:26 -0400

    changed table format, now you can see each set and rep

--- src/components/Routine.jsx
+++ src/components/Routine.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Dropdown, Table } from 'react-bootstrap';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaCalendarAlt, FaEye, FaEyeSlash } from 'react-icons/fa';
 
 function Routine({ date, exercises }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,35 +8,46 @@ function Routine({ date, exercises }) {
   const toggleDropdown = () => setIsOpen(!isOpen);
 
   return (
+    <div className='itemDropdown'>
     <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic" onClick={toggleDropdown}>
-        {date} {isOpen ? <FaEye /> : <FaEyeSlash />}
+        <FaCalendarAlt size='1.5rem' /> {date} {isOpen ? <FaEye /> : <FaEyeSlash />}
       </Dropdown.Toggle>
 
       <Dropdown.Menu show={isOpen}>
         <Dropdown.ItemText>
-          <Table striped bordered hover>
+          <Table bordered hover size="sm">
             <thead>
               <tr>
-                <th>Nombre</th>
-                <th>Equipo</th>
-                <th>Peso</th>
-                <th>Reps</th>
-                <th>Sets</th>
+                <th>Ejercicio</th>
                 <th>Hora</th>
-                <th>Estado</th>
+                <th style={{ minWidth: "75px" }}>Equipo</th>
+                <th>Peso</th>
+                <th style={{ minWidth: "75px" }}>Sets</th>
+                <th style={{ minWidth: "75px" }}>Reps</th>
               </tr>
             </thead>
             <tbody>
               {exercises.map((exercise, index) => (
                 <tr key={index}>
                   <td>{exercise.nombre_ejercicio}</td>
+                  <td>{exercise.hora}</td>
                   <td>{exercise.equipo}</td>
                   <td>{exercise.peso}</td>
-                  <td>{exercise.repeticiones}</td>
-                  <td>{exercise.sets}</td>
-                  <td>{exercise.hora}</td>
-                  <td>{exercise.mood}</td>
+                  <td>
+                    {exercise.sets.map((set, setIndex) => (
+                      <div key={setIndex}>
+                        Set {set.num}
+                      </div>
+                    ))}
+                  </td>
+                  <td>
+                    {exercise.sets.map((set, setIndex) => (
+                      <div key={setIndex}>
+                        {set.reps} reps
+                      </div>
+                    ))}
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -44,6 +55,7 @@ function Routine({ date, exercises }) {
         </Dropdown.ItemText>
       </Dropdown.Menu>
     </Dropdown>
+    </div>
   );
 }
 
